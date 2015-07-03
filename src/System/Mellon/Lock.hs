aefa5e4d1935b76d97a050dbebd36760d87b9ab8
Drew Hess <src@drewhess.com>
2015-07-02 22:53:50 -0700

    Remove outdated comment in Lock.hs.

--- src/System/Mellon/Lock.hs
+++ src/System/Mellon/Lock.hs
@@ -6,9 +6,6 @@ import Control.Monad.IO.Class
 
 -- | 'Lock' abstracts the machinery which manipulates the actual lock.
 -- The protocol is the simplest one possible: 'lock' and 'unlock'.
---
--- Presumably only an effectful lock is useful, therefore 'Lock' is
--- parameterized on 'Monad'.
 class Lock l where
   lock :: MonadIO m => l -> m ()
   unlock :: MonadIO m => l -> m ()
