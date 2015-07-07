aa11e8c5eaf1300d771eba7d35fa52c63fe38884
Drew Hess <src@drewhess.com>
2015-07-07 11:54:34 -0700

    Add missing file from previous commit.

---
+++ src/System/Mellon/Controller.hs
@@ -0,0 +1,9 @@
+module System.Mellon.Controller
+         ( module System.Mellon.Controller.Controller
+         , module System.Mellon.Controller.ThreadedController
+         , module System.Mellon.Controller.TimedController
+         ) where
+
+import System.Mellon.Controller.Controller
+import System.Mellon.Controller.ThreadedController
+import System.Mellon.Controller.TimedController
