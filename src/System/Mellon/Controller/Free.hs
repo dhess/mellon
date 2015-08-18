9098cd5db2193da7a5e6a9d88bc36de9a1ab7bec
Drew Hess <src@drewhess.com>
2015-08-17 19:30:39 -0700

    Add docs for ControllerT and friends.

--- src/System/Mellon/Controller/Free.hs
+++ src/System/Mellon/Controller/Free.hs
@@ -1,9 +1,16 @@
+-- | In @mellon@, a 'ControllerT' is the interface between the user
+-- program and thea @mellon@
+-- 'System.Mellon.StateMachine.StateMachineT'. A useful controller (as
+-- opposed to one used only for debugging or testing) will combine a
+-- 'System.Mellon.Lock.MonadLock' implementation with an
+-- implementation of the 'System.Mellon.StateMachine.StateMachineF'
+-- EDSL to provide the user with a mechanism for locking an unlocking
+-- a physical device according to the @mellon@ state machine protocol.
+--
+
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TemplateHaskell #-}
-
--- | A @mellon@ controller expressed as a 'Control.Monad.Free.Free'
--- monad.
 
 module System.Mellon.Controller.Free
        ( Controller
@@ -18,7 +25,7 @@ import Control.Monad.Free.TH (makeFreeCon)
 import Data.Functor.Identity (Identity)
 import Data.Time (UTCTime)
 
--- | The controller eDSL.
+-- | The (quite simple) 'ControllerT' embedded DSL.
 data ControllerF next where
   LockNow :: next -> ControllerF next
   UnlockUntil :: UTCTime -> next -> ControllerF next
@@ -27,14 +34,25 @@ instance Functor ControllerF where
   fmap f (LockNow x) = LockNow (f x)
   fmap f (UnlockUntil d x) = UnlockUntil d (f x)
 
--- | A controller monad transformer.
+-- | 'ControllerT' is a free monad; controller implementations provide
+-- the machinery for turning the 'ControllerF' EDSL into actions that
+-- transition the @mellon@ 'System.Mellon.StateMachine.StateMachineT'
+-- from one state to the next. A controller implementation is also
+-- responsible for keeping track of the current state of the state
+-- machine.
 type ControllerT = FreeT ControllerF
 
--- | A basic controller. This controller is probably not very useful,
--- except possibly for testing.
+-- | 'Controller' wraps 'ControllerT' around the 'Identity' monad.
+-- This controller is probably not very useful, except possibly for
+-- testing.
 type Controller = ControllerT Identity
 
--- | Lock the controller now.
+-- | Tell the controller to lock its device immediately. Any
+-- previously-executed unlock command that is currently in effect will
+-- be canceled.
 makeFreeCon 'LockNow
--- | Unlock the controller until the given 'UTCTime'.
+-- | Tell the controller to unlock its device until a given 'UTCTime',
+-- at which time the controller will automatically lock the device
+-- again. Note that you can unlock the device indefinitely by
+-- specifying a time in the past.
 makeFreeCon 'UnlockUntil
