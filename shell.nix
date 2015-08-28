084bc034df0df63af00dd9b6ccc3936051344de7
Drew Hess <src@drewhess.com>
2015-08-27 19:55:05 -0700

    Update shell.nix, add default.nix.

--- shell.nix
+++ shell.nix
@@ -1,19 +1,29 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, free, hspec, optparse-applicative, stdenv
-             , text, time, transformers
-             }:
-             mkDerivation {
-               pname = "mellon";
-               version = "0.0.0";
-               src = ./.;
-               isLibrary = true;
-               isExecutable = true;
-               buildDepends = [
-                 base free optparse-applicative text time transformers
-               ];
-               testDepends = [ base free hspec text time transformers ];
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, free, hspec, mtl, optparse-applicative
+      , stdenv, text, time, transformers
+      }:
+      mkDerivation {
+        pname = "mellon";
+        version = "0.1.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        buildDepends = [
+          base free mtl optparse-applicative text time transformers
+        ];
+        testDepends = [ base free hspec mtl text time transformers ];
+        homepage = "https://github.com/dhess/mellon/";
+        description = "Control physical access devices";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  drv = pkgs.haskell.packages.${compiler}.callPackage f {};
+
 in
-  pkg.env
+
+  if pkgs.lib.inNixShell then drv.env else drv
