ae1fffe89d19c61c50f9b7b56e240d3f4a80a565
Drew Hess <src@drewhess.com>
2015-09-17 14:18:37 -0700

    Update Nix files.

--- default.nix
+++ default.nix
@@ -1,16 +1,14 @@
-{ mkDerivation, base, free, hspec, mtl, optparse-applicative
-, stdenv, text, time, transformers
+{ mkDerivation, base, free, hspec, mtl, stdenv, text, time
+, transformers
 }:
 mkDerivation {
   pname = "mellon";
-  version = "0.1.0";
+  version = "0.1.1";
   src = ./.;
-  isLibrary = true;
-  isExecutable = true;
-  buildDepends = [
-    base free mtl optparse-applicative text time transformers
+  libraryHaskellDepends = [ base free mtl text time transformers ];
+  testHaskellDepends = [
+    base free hspec mtl text time transformers
   ];
-  testDepends = [ base free hspec mtl text time transformers ];
   homepage = "https://github.com/dhess/mellon/";
   description = "Control physical access devices";
   license = stdenv.lib.licenses.bsd3;
--- shell.nix
+++ shell.nix
@@ -1,28 +1,30 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
 
 let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, free, hspec, mtl, optparse-applicative
-      , stdenv, text, time, transformers
+  f = { mkDerivation, base, free, hspec, mtl, stdenv, text, time
+      , transformers
       }:
       mkDerivation {
         pname = "mellon";
-        version = "0.1.0";
+        version = "0.1.1";
         src = ./.;
-        isLibrary = true;
-        isExecutable = true;
-        buildDepends = [
-          base free mtl optparse-applicative text time transformers
+        libraryHaskellDepends = [ base free mtl text time transformers ];
+        testHaskellDepends = [
+          base free hspec mtl text time transformers
         ];
-        testDepends = [ base free hspec mtl text time transformers ];
         homepage = "https://github.com/dhess/mellon/";
         description = "Control physical access devices";
         license = stdenv.lib.licenses.bsd3;
       };
 
-  drv = pkgs.haskell.packages.${compiler}.callPackage f {};
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
 
 in
 
