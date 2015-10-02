e8b3cedbe6963294296bb872d3788c5c54b2add2
Drew Hess <src@drewhess.com>
2015-10-01 19:19:40 -0700

    Add mellon dependency in mellon-server.

--- mellon-server/default.nix
+++ mellon-server/default.nix
@@ -1,20 +1,22 @@
-{ mkDerivation, aeson, base, either, hspec, lucid
+{ mkDerivation, aeson, base, either, hspec, lucid, mellon
 , optparse-applicative, servant, servant-docs, servant-lucid
 , servant-server, stdenv, text, time, transformers, wai, warp
 }:
 mkDerivation {
   pname = "mellon-server";
-  version = "0.0.1";
+  version = "0.1.1";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson base either lucid optparse-applicative servant servant-docs
-    servant-lucid servant-server text time transformers wai warp
+    aeson base either lucid mellon optparse-applicative servant
+    servant-docs servant-lucid servant-server text time transformers
+    wai warp
   ];
   executableHaskellDepends = [
-    aeson base either lucid optparse-applicative servant servant-docs
-    servant-lucid servant-server text time transformers wai warp
+    aeson base either lucid mellon optparse-applicative servant
+    servant-docs servant-lucid servant-server text time transformers
+    wai warp
   ];
   testHaskellDepends = [ base hspec ];
   license = stdenv.lib.licenses.bsd3;
