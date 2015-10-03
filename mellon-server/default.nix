1a80313cbdce76f46dadb2555d0479521f20f84d
Drew Hess <src@drewhess.com>
2015-10-03 15:33:39 -0700

    Bump versions.

--- mellon-server/default.nix
+++ mellon-server/default.nix
@@ -4,7 +4,7 @@
 }:
 mkDerivation {
   pname = "mellon-server";
-  version = "0.1.1";
+  version = "0.2.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
--- mellon/default.nix
+++ mellon/default.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "mellon";
-  version = "0.1.1";
+  version = "0.2.0";
   src = ./.;
   libraryHaskellDepends = [ base free mtl text time transformers ];
   testHaskellDepends = [
