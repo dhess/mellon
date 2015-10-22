1e8958921f601b6968e2ed3481e88ac4449833f2
Drew Hess <src@drewhess.com>
2015-10-22 15:42:25 -0700

    Add hspec-wai and hspec-wai-json dependencies.

--- mellon-server/default.nix
+++ mellon-server/default.nix
@@ -1,6 +1,7 @@
-{ mkDerivation, aeson, base, bytestring, either, hspec, http-client
-, http-types, lucid, mellon, servant, servant-docs, servant-lucid
-, servant-server, stdenv, text, time, transformers, wai, warp
+{ mkDerivation, aeson, base, bytestring, either, hspec, hspec-wai
+, hspec-wai-json, http-client, http-types, lucid, mellon, servant
+, servant-docs, servant-lucid, servant-server, stdenv, text, time
+, transformers, wai, warp
 }:
 mkDerivation {
   pname = "mellon-server";
@@ -12,9 +13,9 @@ mkDerivation {
     wai warp
   ];
   testHaskellDepends = [
-    aeson base bytestring either hspec http-client http-types lucid
-    mellon servant servant-docs servant-lucid servant-server text time
-    transformers wai warp
+    aeson base bytestring either hspec hspec-wai hspec-wai-json
+    http-client http-types lucid mellon servant servant-docs
+    servant-lucid servant-server text time transformers wai warp
   ];
   description = "A REST web service for mellon";
   license = stdenv.lib.licenses.bsd3;
