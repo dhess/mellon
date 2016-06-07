699a64afd0c14e145456d4d94b162b9e6627ed24
Drew Hess <src@drewhess.com>
2016-06-06 17:37:15 -0700

    mellon-web Client.hs cleanup.
    * Better documentation.
    
    * Import hygiene.
    
    * Don't export 'clientAPI'; it's not very useful outside this module.

--- mellon-web/src/Mellon/Web/Client.hs
+++ mellon-web/src/Mellon/Web/Client.hs
@@ -1,11 +1,17 @@
 {-|
 Module      : Mellon.Web.Client
-Description : Client actions for the Mellon REST web service
+Description : Client actions for the REST web service
 Copyright   : (c) 2016, Drew Hess
 License     : BSD3
 Maintainer  : Drew Hess <src@drewhess.com>
 Stability   : experimental
 Portability : non-portable
+
+This module provides client-side actions for interacting with the
+server-side 'MellonAPI'.
+
+The client actions are implemented on top of the "Network.HTTP.Client"
+and "Servant.Client" modules.
 
 -}
 
@@ -13,21 +19,33 @@ Portability : non-portable
 {-# LANGUAGE TypeOperators #-}
 
 module Mellon.Web.Client
-         ( -- * Re-exported for convenience.
-           State(..)
-         , Time(..)
-           -- * Client functions
-         , clientAPI
-         , getTime
+         ( -- * Client actions
+           --
+           -- | These actions take a 'Manager' and a 'BaseUrl', and
+           -- should then be run in an 'ExceptT' transformer stack to
+           -- produce a result. For example, assuming the server is
+           -- running on @localhost@ on port @8081@:
+           --
+           -- > > let baseUrl = BaseUrl Http "localhost" 8081 ""
+           -- > > manager <- newManager defaultManagerSettings
+           -- > > runExceptT $ putState Locked manager baseUrl
+           -- > Right Locked
+           getTime
          , getState
          , putState
+
+           -- * Server types
+           --
+           -- | Re-exported for convenience.
+         , State(..)
+         , Time(..)
          ) where
 
 import Control.Monad.Trans.Except (ExceptT)
-import Data.Proxy
+import Data.Proxy (Proxy(..))
 import Network.HTTP.Client (Manager)
-import Servant.API
-import Servant.Client
+import Servant.API ((:<|>)(..))
+import Servant.Client (BaseUrl, ServantError, client)
 
 import Mellon.Web.Server (MellonAPI, State(..), Time(..))
 
@@ -35,9 +53,15 @@ import Mellon.Web.Server (MellonAPI, State(..), Time(..))
 clientAPI :: Proxy MellonAPI
 clientAPI = Proxy
 
--- | Generate the client functions for a given host and URL
--- scheme.
+-- | Get the server's time. This action is provided chiefly to verify
+-- the accuracy of the server's clock.
 getTime :: Manager -> BaseUrl -> ExceptT ServantError IO Time
+
+-- | Get the current state of the server's
+-- 'Mellon.Controller.Controller'.
 getState :: Manager -> BaseUrl -> ExceptT ServantError IO State
+
+-- | Lock or unlock the server's 'Mellon.Controller.Controller'.
 putState :: State -> Manager -> BaseUrl -> ExceptT ServantError IO State
+
 getTime :<|> getState :<|> putState = client clientAPI
