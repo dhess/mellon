ea387309c941fc36317a1af38aa85422254ddc4e
Drew Hess <src@drewhess.com>
2016-06-09 16:14:47 -0700

    Remove placeholder doctests.
    Weren't using them in mellon-gpio or mellon-web, and they cause problems
    in Nix. (See, e.g.,
    http://lists.science.uu.nl/pipermail/nix-dev/2014-November/014945.html)
    
    There are work-arounds for Nix, but as we're not using these doctests
    anyway, I'll just remove them for now.

--- mellon-gpio/default.nix
+++ mellon-gpio/default.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, base, doctest, hlint, hpio, mellon-core, stdenv }:
+{ mkDerivation, base, hlint, hpio, mellon-core, stdenv }:
 mkDerivation {
   pname = "mellon-gpio";
   version = "0.6.0.1";
   src = ./.;
   libraryHaskellDepends = [ base hpio mellon-core ];
-  testHaskellDepends = [ base doctest hlint ];
+  testHaskellDepends = [ base hlint ];
   homepage = "https://github.com/dhess/mellon/";
   description = "GPIO support for mellon";
   license = stdenv.lib.licenses.bsd3;
--- mellon-web/default.nix
+++ mellon-web/default.nix
@@ -1,9 +1,8 @@
-{ mkDerivation, aeson, base, bytestring, doctest, exceptions, hlint
-, hpio, hspec, hspec-wai, http-client, http-types, lucid
-, mellon-core, mellon-gpio, mtl, network, optparse-applicative
-, servant, servant-client, servant-docs, servant-lucid
-, servant-server, stdenv, text, time, transformers, wai, wai-extra
-, warp
+{ mkDerivation, aeson, base, bytestring, exceptions, hlint, hpio
+, hspec, hspec-wai, http-client, http-types, lucid, mellon-core
+, mellon-gpio, mtl, network, optparse-applicative, servant
+, servant-client, servant-docs, servant-lucid, servant-server
+, stdenv, text, time, transformers, wai, wai-extra, warp
 }:
 mkDerivation {
   pname = "mellon-web";
@@ -21,10 +20,10 @@ mkDerivation {
     optparse-applicative transformers warp
   ];
   testHaskellDepends = [
-    aeson base bytestring doctest hlint hspec hspec-wai http-client
-    http-types lucid mellon-core network servant servant-client
-    servant-docs servant-lucid servant-server text time transformers
-    wai wai-extra warp
+    aeson base bytestring hlint hspec hspec-wai http-client http-types
+    lucid mellon-core network servant servant-client servant-docs
+    servant-lucid servant-server text time transformers wai wai-extra
+    warp
   ];
   homepage = "https://github.com/dhess/mellon/";
   description = "A REST web service for Mellon controllers";
